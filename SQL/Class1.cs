73a5809e06976e557cefab573ba8ce46536888e2
DrEdgarvillegas <edgarvillegas5@gmail.com>
2022-05-13 09:12:54 -0300

    SQL Conection

---
+++ SQL/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace SQL
+{
+    class conexionbd
+    {
+        string cadena = "Data Source=DESKTOP-T9873S7\\SQLEXPRESS;Initial Catalog=Pruebabd; Integrated Security =True";
+        public SqlConnection conectarbd = new SqlConnection();
+
+        public conexionbd() 
+        {
+            conectarbd.ConnectionString = cadena;
+        }
+
+        public void abrir() 
+        {
+            try
+            {
+                conectarbd.Open();
+                Console.WriteLine("Conexión establecida");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Error al conectar la base de datos "+ ex.Message);
+            }
+        }
+
+        public void cerrar() 
+        {
+            conectarbd.Close();
+        }
+
+
+    }
+}
---
+++ SQL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            conexionbd conexion = new conexionbd();
+            conexion.abrir();
+            conexion.cerrar();
+
+
+           
+        }
+    }
+}
